85103a8cf975f2b4664373c0be921ec89bebea27
Timothy OGrady <timothyogrady@Mac.attlocal.net>
2025-11-23 21:35:08 -0600

    Fix Tailwind content paths in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
