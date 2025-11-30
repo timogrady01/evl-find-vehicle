849c4ebb830a988bba911b8e0b1bc288a7996040
Timothy OGrady <timothyogrady@Mac.attlocal.net>
2025-11-30 15:40:53 -0600

    Clean Vite structure for EVL

--- src/Components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-200 text-center p-4">
-      © 2025 EVL. All rights reserved.
-    </footer>
-  );
-}
--- src/Components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="bg-gray-900 text-white p-4 text-center text-xl">
-      EVL Platform
-    </header>
-  );
-}
