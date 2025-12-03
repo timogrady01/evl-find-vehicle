eb88c1927adf812737bb718bcf2360060fa8b2db
timogrady01 <tim_ogrady@yahoo.com>
2025-12-03 10:27:49 -0600

    Add vehicles data to vehicles.ts

---
+++ src/data/vehicles.ts
@@ -0,0 +1,7 @@
+
+export const vehicles = [
+  { id: 1, make: "Toyota", model: "Camry", trim: "XSE", year: 2024 },
+  { id: 2, make: "Honda", model: "Accord", trim: "Sport", year: 2024 },
+  { id: 3, make: "Ford", model: "F-150", trim: "Lariat", year: 2023 },
+  { id: 4, make: "Chevrolet", model: "Silverado", trim: "LTZ", year: 2023 },
+  { id: 5, make: "Tesla", model: "Model 3", trim: "Performance", year: 2024 },
