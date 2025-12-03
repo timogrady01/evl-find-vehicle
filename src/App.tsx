ff516d50c056d0e62d0238a48112787ca0728d10
timogrady01 <tim_ogrady@yahoo.com>
2025-12-03 14:38:43 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import Home from './pages/Home';
 
 const App: React.FC = () => {
-  return <Home />;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 };
 
 export default App;
